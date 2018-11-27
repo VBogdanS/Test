7839be8298ffd38c68a526e090b6817568ac9c48
VBogdanS <fcsbogdan@gmail.com>
2018-11-27 14:43:42 +0200

    prima versiune

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+	include_once 'includes/header.php'
+?>
+
+    <div class="content">
+		<!--<p><h3>钱 (Qian) 南翔 (Nanxiang)</h3></p> -->
+      <p><h4>A sentence with <b>bold, <i>italic</i></b> text.</h4></p>
+    	
+    </div> <!--end content-->
+    
+<?php
+	include_once 'includes/footer.php'
+?>    
